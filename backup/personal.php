8bc73ddf996b6cae541584d05097ea89f6063fbd
DESKTOP-SG3VV5Q\Damian <damiandib99@gmail.com>
2017-05-29 14:20:48 +0200

    Hackathon geweest

---
+++ backup/personal.php
@@ -0,0 +1,41 @@
+<?php
+#timetimer, agenda, profielfoto
+?>
+<html>
+<head>
+    <title></title>
+    <link href="stylesheet.css" type="text/css" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<nav>
+    <div class="center-align nav-wrapper brown">
+        <a href="#" class="brand-logo">Persoonlijke agenda</a>
+    </div>
+</nav>
+<div class="vspacer"></div>
+<div class="timesection">
+    <image class="circle profpicture" src="images/sander.jpg"></image>
+</div>
+<div class="agendasection">
+    <div class="row">
+        <div class="daypart">
+            <image src="images/pic/ochtend.png" style="height:100%; width:auto;"></image>
+        </div>
+    </div>
+    <div class="row">
+        <div class="daypart">
+            <image src="images/pic/middag.png" style="height:100%; width:auto;"></image>
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="daypart">
+            <image src="images/pic/avond.png" style="height:100%; width:auto;"></image>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
